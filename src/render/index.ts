a956909796a3a054fd8160fc029f42faec02fc66
rusnick31 <rusnick31@gmail.com>
2019-08-28 13:53:11 +0300

    render method refactored

--- src/render/index.ts
+++ src/render/index.ts
@@ -1,17 +1,10 @@
 import { reconcile } from './helpers';
 
 let virtualDom: Instance = null;
+
 function render(element: string | CustomElement, container: HTMLElement) {
-
-  if (!virtualDom) {
-    const instance = reconcile(virtualDom, element);
-    virtualDom = instance;
-    container.append(instance.dom);
-  }
-
-  const instance = reconcile(virtualDom, element);
+  const instance = reconcile(container, virtualDom, element);
   virtualDom = instance;
-  
 }
 
 export default render;
